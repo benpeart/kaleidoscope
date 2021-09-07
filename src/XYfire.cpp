a139f8fe8e187724ecc2cb8887e92d6158839977
Ben Peart <benpeart@github.com>
2021-09-07 17:11:07 -0400

    An even better looking fire with less code - win, win!

--- src/XYfire.cpp
+++ src/XYfire.cpp
@@ -5,78 +5,31 @@
 
 #ifdef DEMO
 
-DEFINE_GRADIENT_PALETTE(firePal_gp){
-    0, 0, 0, 0,          //black
-    100, 100, 0, 0,      //dark red
-    128, 255, 0, 0,      //red
-    220, 255, 255, 0,    //yellow
-    255, 255, 255, 255}; //white
-
-static uint8_t firePixels[NUM_STRIPS * NUM_LEDS_PER_STRIP];
-static CRGBPalette16 _currentPalette = firePal_gp;
-static boolean initialized = false;
-
-void spreadFire(uint16_t src)
-{
-    if (firePixels[src] == 0)
-    {
-        firePixels[src - NUM_COLS] = 0;
-    }
-    else
-    {
-        // Commented lines moves fire sideways as well as up, but doesn't look good on low res matrix:
-        // int16_t dst = src - rand + 1;
-        // firePixels[dst - NUM_COLS] = firePixels[src] - random8(1);
-        firePixels[src - NUM_COLS] = firePixels[src] - random8(3);
-    }
-}
-
-void doFire()
-{
-    for (uint16_t x = 0; x < NUM_COLS; x++)
-    {
-        for (uint16_t y = 1; y < NUM_ROWS; y++)
-        {
-            spreadFire(y * NUM_COLS + x);
-        }
-    }
-}
-
-void setBottomRow(uint16_t col)
-{
-    for (uint16_t i = 0; i < NUM_COLS; i++)
-    {
-        firePixels[(NUM_ROWS - 1) * NUM_COLS + i] = col;
-    }
-}
-
-// https://github.com/s-marley/LEDMask/blob/master/MaskUpdate/Fire.h
+byte scale = 64;
+byte speed = 92;
+static uint32_t t;
 void mode_xy_fire()
 {
-    // Set bottom row to highest index in palette (white)
-    if (!initialized)
+    EVERY_N_MILLISECONDS(100)
     {
-        setBottomRow(NUM_ROWS);
-        initialized = true;
-    }
-
-    EVERY_N_MILLISECONDS(50)
-    {
-        doFire();
-        for (int y = 0; y < NUM_ROWS; y++)
+        t += speed;
+        for (byte x = 0; x < NUM_COLS; x++)
         {
-            for (int x = 0; x < NUM_COLS; x++)
+            for (byte y = 0; y < NUM_ROWS; y++)
             {
-                int index = firePixels[NUM_COLS * y + x];
-                // Index goes from 0 -> NUM_ROWS, palette goes from 0 -> 255 so need to scale it
-                uint8_t indexScale = 255 / NUM_ROWS;
-                leds[XYToIndex(x, y)] = ColorFromPalette(_currentPalette, constrain(index * indexScale, 0, 255), 255, LINEARBLEND);
+
+                int16_t Bri = inoise8(x * scale, (y * scale) - t) - (y * (255 / NUM_ROWS));
+                byte Col = Bri; // inoise8(x * scale, (y * scale) - t) - (y * (255 / NUM_ROWS));
+                if (Bri < 0)
+                    Bri = 0;
+                if (Bri != 0)
+                    Bri = 256 - (Bri * 0.2);
+                nblend(leds[XYToIndex(x, NUM_ROWS - y)], ColorFromPalette(HeatColors_p, Col, Bri), speed);
             }
         }
 
         leds_dirty = true;
     }
-
     adjustBrightness();
 }
 
